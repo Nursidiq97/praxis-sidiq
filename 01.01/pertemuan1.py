13da33ea703c25980c72d91109a0ee1a3e5a20fd
nursidiq97 <sidiqnur363@gmail.com>
2021-10-04 14:06:49 +0700

    edit file pertemuan yang pertama untuk yang ketiga kalinya

--- 01.01/pertemuan1.py
+++ 01.01/pertemuan1.py
@@ -1,3 +1,4 @@
 print (" Hello Word")
 print("nama Saya adalaha Nur Sidiq gaul seduania")
 print("nama Saya adalaha Nur Sidiq gaul seduania raya sentosa")
+print("bio saya adalah membangun diri sendiri untuk membantu orang lain")
