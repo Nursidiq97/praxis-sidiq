8323e0689ebce2839a4ffc63c013d14e6b5a5076
nursidiq97 <sidiqnur363@gmail.com>
2021-10-05 13:19:17 +0700

    edit file pemrogramandasar yang pertama kalinya

---
+++ 01-02/pemrogamandasar.py
@@ -0,0 +1,33 @@
+#string 
+x = 'ini adalah string'
+print(type(x))
+print(x)
+
+#integer 
+b = 100
+print(type(b))
+print(b)
+
+#boolean 
+c = False
+print(type(c))
+
+#list
+x =["pisang", "nangka", "jeruk",]
+print(type(x))
+print(x)
+
+#range
+x = 6
+print(type(x)) 
+print(6)
+
+#Dictonary
+Biodata = {
+    "Nama"   : "Nur Sidiq",
+    "Prodi"  : "Informatika",
+    "Alamat" : "Muara Sugihan, Sumsel"
+}
+print  (Biodata)
+print(type (Biodata))
+
