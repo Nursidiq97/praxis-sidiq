620a18074ba5a304ad631e8e2b9d98a1fac80632
nursidiq97 <sidiqnur363@gmail.com>
2021-10-06 15:59:02 +0700

    Pertemuan Ketiga Kalinya

--- 01-03/mysite/tugas/views.py
+++ 01-03/mysite/tugas/views.py
@@ -6,4 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hallo, Nama Saya Nur Sidiq, Asal Dari Pelembang.")
-    return HttpResoonse("Jangan Lupa Bahagia")
+
+
+
